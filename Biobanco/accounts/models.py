eea4103b2a08eab0f16679fbedb5081f56225575
cdclkm <ca.campusano@duocuc.cl>
2023-11-22 11:23:37 -0300

    creación de grupo supervisor y técnico en models.py. / en signup.html, cambio de label en checkbox

--- Biobanco/accounts/models.py
+++ Biobanco/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.db.models.query import QuerySet
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class MyAccountManager(BaseUserManager):
 
@@ -31,8 +33,20 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
 
     def get_by_natural_key(self, email):
-        return self.get(email=self.normalize_email(email))
+        return self.get(email=self.normalize_email(email)) 
+    
+    def __str__(self):
+        return self.username
 
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+#is_admin: Administrador
+#is_staff: personal Técnico
+#is_superuser: Supervisor 
 
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
@@ -50,12 +64,77 @@ class Account(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+    
+    #groups(roles) for users
+    class Role(models.TextChoices):
+        ADMIN = "Administrador", "Administrador"
+        SUPERVISOR = "Supervisor", "Supervisor"
+        TECNICO = "Tecnico", "Técnico"
+        
 
-    def __str__(self):
-        return self.username
+    base_role = Role.OTHER 
 
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
+    role = models.CharField(max_length=50, choices=Role.choices)
 
-    def has_module_perms(self, app_label):
-        return True
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **args)
+    
+#group supervisor
+class SupervisorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=Account.Role.SUPERVISOR)
+
+class Supervisor(Account):
+
+    base_role = Account.Role.SUPERVISOR
+
+    supervisor = SupervisorManager() 
+
+    class Meta:
+        proxy = True
+    
+    def tipo_role(self):
+        return "Sólo para rol de Supervisor" 
+    
+#@receiver(post_save, sender=Supervisor)
+#def create_account_profile(sender, instance, created, **kwargs):
+#    if created and instance.role == "SUPERVISOR":
+#        SupervisorProfile.objects.create(user=instance)
+
+#class SupervisorProfile(models.Model): 
+#   user = models.OneToOneField(Account, on_delete=models.CASCADE)
+#   supervisor_id = models.IntegerField(null=True, blank=True)
+
+
+
+#group técnico
+class TecnicoManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=Account.Role.TECNICO)
+
+class Tecnico(Account):
+
+    base_role = Account.Role.TECNICO
+
+    tecnico = TecnicoManager() 
+
+    class Meta:
+        proxy = True
+    
+    def tipo_role(self):
+        return "Sólo para rol de Técnico" 
+    
+#@receiver(post_save, sender=Tecnico)
+#def create_account_profile(sender, instance, created, **kwargs):
+#   if created and instance.role == "TECNICO":
+#        TecnicoProfile.objects.create(user=instance)
+
+#class TecnicoProfile(models.Model): 
+#   user = models.OneToOneField(Account, on_delete=models.CASCADE)
+#   tecnico_id = models.IntegerField(null=True, blank=True)
+
+
